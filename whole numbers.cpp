651105169794bbf5b436c70f768c9955965b14e8
Saivarshitha29 <112745325+Saivarshitha29@users.noreply.github.com>
2023-02-02 17:43:19 +0530

    Add files via upload

---
+++ whole numbers.cpp
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+	int a=1;
+	int sum=0;
+	int num;
+	printf("enter the  maximum number");
+	scanf("%d",&num);
+	printf("the odd numbers in the list are");
+	while(a<=num)
+	
+	printf("%d", a);
+	sum=sum+a;
+	a=a+2;
+	{
+		printf("sum of all odd numbers =%d ,n", sum);
+		}	
+}
